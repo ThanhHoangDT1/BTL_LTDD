9152c793dcd5bd90e80bebd5edc9dd83c6a445b7
Quoc <nguyenchiquocit2002@gmail.com>
2023-03-23 10:33:00 +0700

    Chuyển đổi giao diện

--- app/src/main/java/com/androidexam/cafeapp/LoginActivity.java
+++ app/src/main/java/com/androidexam/cafeapp/LoginActivity.java
@@ -2,13 +2,39 @@ package com.androidexam.cafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnSignin;
+    private TextView tvSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignin = (Button) findViewById(R.id.buttonSignIn);
+        tvSingUp = (TextView) findViewById(R.id.textSignUp);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tvSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/androidexam/cafeapp/MainActivity.java
+++ app/src/main/java/com/androidexam/cafeapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidexam.cafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+
     }
 }
